4134724617b00a41820804489d51f496d6cd5936
shenckens <stijn.henckens@gmai.com>
2022-06-28 16:06:22 +0200

    missing loss module

--- models/roomrecon_network.py
+++ models/roomrecon_network.py
@@ -132,8 +132,256 @@ class RoomNet(nn.Module):
         return up_feat, up_coords
 
     def forward(self, features, inputs, outputs):
-        return 0
+        '''
 
-    def compute_loss(self):
-        loss = 0
+        :param features: list: features for each image: eg. list[0] : pyramid features for image0 : [(B, C0, H, W), (B, C1, H/2, W/2), (B, C2, H/2, W/2)]
+        :param inputs: meta data from dataloader
+        :param outputs: {}
+        :return: outputs: dict: {
+            'coords':                  (Tensor), coordinates of voxels,
+                                    (number of voxels, 4) (4 : batch ind, x, y, z)
+            'tsdf':                    (Tensor), TSDF of voxels,
+                                    (number of voxels, 1)
+        }
+        :return: loss_dict: dict: {
+            'tsdf_occ_loss_X':         (Tensor), multi level loss
+        }
+        '''
+        bs = features[0][0].shape[0]
+        if "plane_anchors" in inputs.keys():
+            anchors_gt = inputs['plane_anchors']
+            residual_gt = inputs['residual']
+            planes_gt = inputs['planes_trans']
+            mean_xyz_gt = inputs['mean_xyz']
+        else:
+            anchors_gt = residual_gt = planes_gt = mean_xyz_gt = None
+
+        pre_feat = None
+        pre_coords = None
+        outputs['label_target'] = []
+        outputs['occ_target'] = []
+        outputs['coords_'] = []
+        loss_dict = {}
+        # ----coarse to fine----
+        for i in range(self.cfg.N_LAYER):
+            interval = 2 ** (self.n_scales - i)
+            scale = self.n_scales - i
+
+            if i == 0:
+                # ----generate new coords----
+                coords = generate_grid(self.cfg.N_VOX, interval)[0]
+                up_coords = []
+                for b in range(bs):
+                    up_coords.append(
+                        torch.cat([torch.ones(1, coords.shape[-1]).to(coords.device) * b, coords]))
+                up_coords = torch.cat(
+                    up_coords, dim=1).permute(1, 0).contiguous()
+            else:
+                # ----upsample coords----
+                up_feat, up_coords = self.upsample(
+                    pre_feat, pre_coords, interval)
+
+            # ----back project----
+            feats = torch.stack([feat[scale] for feat in features])
+            KRcam = inputs['proj_matrices'][:, :, scale].permute(
+                1, 0, 2, 3).contiguous()
+            volume, count = back_project(up_coords, inputs['vol_origin_partial'],
+                                         self.cfg.VOXEL_SIZE, feats, KRcam)
+            grid_mask = count > 1
+
+            # ----concat feature from last stage----
+            if i != 0:
+                feat = torch.cat([volume, up_feat], dim=1)
+            else:
+                feat = volume
+
+            if not self.cfg.FUSION.FUSION_ON:
+                tsdf_target, label_target, occ_target = self.get_target(up_coords, inputs, scale)
+
+            # ----convert to aligned camera coordinate----
+            r_coords = up_coords.detach().clone().float()
+            for b in range(bs):
+                batch_ind = torch.nonzero(up_coords[:, 0] == b).squeeze(1)
+                coords_batch = up_coords[batch_ind][:, 1:].float()
+                coords_batch = coords_batch * self.cfg.VOXEL_SIZE + \
+                    inputs['vol_origin_partial'][b].float()
+                coords_batch = torch.cat(
+                    (coords_batch, torch.ones_like(coords_batch[:, :1])), dim=1)
+                coords_batch = coords_batch @ inputs['world_to_aligned_camera'][b, :3, :].permute(
+                    1, 0).contiguous()
+                r_coords[batch_ind, 1:] = coords_batch
+
+            # batch index is in the last position
+            r_coords = r_coords[:, [1, 2, 3, 0]]
+
+            # ----sparse conv 3d backbone----
+            point_feat = PointTensor(feat, r_coords)
+            feat = self.sp_convs[i](point_feat)
+
+            # ----gru fusion----
+            if self.cfg.FUSION.FUSION_ON:
+                up_coords, feat, tsdf_target, occ_target = self.gru_fusion(
+                    up_coords, feat, inputs, i)
+                if self.cfg.FUSION.FULL:
+                    grid_mask = torch.ones_like(feat[:, 0]).bool()
+                # ---- edited below from PR ----
+                if label_target is not None:
+                    label_target = label_target.squeeze(1).long()
+                    occ_target = occ_target.squeeze(1)
+
+            tsdf = self.tsdf_preds[i](feat)
+            occ = self.occ_preds[i](feat)
+            # class and regress plane
+            class_logits = self.plane_class[i](feat)
+            residuals = self.plane_residual[i](feat)
+            residuals = residuals.view(-1, 7, 3)
+            distance = self.plane_distance[i](feat)
+            if i == self.n_scales:
+                off_center = self.offset_center_preds(feat)
+            else:
+                off_center = None
+
+
+            # ------define the sparsity for the next stage-----
+            occupancy = occ.squeeze(1) > self.cfg.THRESHOLDS[i]
+            occupancy[grid_mask == False] = False
+
+            num = int(occupancy.sum().data.cpu())
+
+            # -------compute loss-------
+            if tsdf_target is not None and anchors_gt is not None and self.training:
+                loss = 0
+                # loss = self.compute_loss(tsdf, occ, tsdf_target, occ_target,
+                #                          mask=grid_mask,
+                #                          pos_weight=self.cfg.POS_WEIGHT)
+            else:
+                loss = torch.Tensor(np.array([0]))[0]
+            loss_dict.update({f'combined_loss_{i}': loss})
+
+            if num == 0:
+                logger.warning('no valid points: scale {}'.format(i))
+                return outputs, loss_dict
+
+            # ------avoid out of memory: sample points if num of points is too large-----
+            if self.training and num > self.cfg.TRAIN_NUM_SAMPLE[i] * bs:
+                choice = np.random.choice(num, num - self.cfg.TRAIN_NUM_SAMPLE[i] * bs,
+                                          replace=False)
+                ind = torch.nonzero(occupancy)
+                occupancy[ind[choice]] = False
+
+            pre_coords = up_coords[occupancy]
+            for b in range(bs):
+                batch_ind = torch.nonzero(pre_coords[:, 0] == b).squeeze(1)
+                if len(batch_ind) == 0:
+                    logger.warning(
+                        'no valid points: scale {}, batch {}'.format(i, b))
+                    return outputs, loss_dict
+
+            pre_feat = feat[occupancy]
+            pre_tsdf = tsdf[occupancy]
+            pre_occ = occ[occupancy]
+            pre_class = class_logits[occupancy]
+            pre_distance = distance[occupancy]
+
+            # ---- Concatenate all new features for next layer. ----
+            pre_feat = torch.cat([pre_feat, pre_tsdf, pre_occ, pre_class, pre_distance], dim=1)
+
+            output_coord_ = pre_coords.detach().clone()
+            output_coord_[:, 1:] = output_coord_[:, 1:] // 2 ** scale
+            outputs['coords_'].append(output_coord_.long())
+
+            if anchors_gt is not None:
+                outputs['label_target'].append(label_target[occupancy])
+                outputs['occ_target'].append(occ_target[occupancy])
+
+            if i == self.cfg.N_LAYER - 1:
+
+                # ----convert class, residuals to normals -----
+                class_probs = F.softmax(class_logits[occupancy], dim=-1)
+                class_ids = class_probs.argmax(-1)
+                idx = torch.arange(class_ids.shape[0], device=class_ids.device).long()
+                residuals_pred = residuals[occupancy][idx, class_ids]
+                normals = self.normal_anchors[class_ids] + residuals_pred
+                offset_points = r_coords[occupancy, :3] + 0.12 * distance[occupancy] * normals / torch.norm(normals, dim=1,
+                                                                                                     keepdim=True)
+                D = -(offset_points.unsqueeze(1) @ normals.unsqueeze(2)).squeeze(1)
+                planes = torch.cat([normals, D], dim=1)
+
+                center_points = r_coords[occupancy, :3] + off_center[occupancy]
+                center_points = torch.cat((center_points, torch.ones_like(center_points[:, :1])), dim=1)
+                for b in range(bs):
+                    # convert coordinate
+                    ind = torch.nonzero(pre_coords[:, 0] == b, as_tuple=False).squeeze(1)
+                    planes[ind] = (inputs['world_to_aligned_camera'][b].transpose(0,
+                                                                            1) @ planes[ind].transpose(
+                        0, 1)).transpose(0, 1)
+                    if anchors_gt is not None:
+                        planes_gt[b] = (inputs['world_to_aligned_camera'][b].transpose(0,
+                                                                                    1) @ planes_gt[b].transpose(
+                            0, 1)).transpose(0, 1)
+
+                    center_points[ind] = center_points[ind] @ torch.inverse(inputs['world_to_aligned_camera'])[b].permute(1, 0).contiguous()
+                center_points = center_points[:, :3]
+
+                # A,B,C,D,X,Y,Z,OCC for vote
+                # planes = planes[:, :3] # / planes[:, 3:]
+                embedding = torch.cat([planes[:, :3], center_points, planes[:, 3:], pre_occ], dim=1)
+
+                outputs['embedding'] = embedding
+                outputs['planes_gt'] = planes_gt
+                outputs['feat'] = feat
+
+                # --- Coming from NR ----
+                outputs['coords'] = pre_coords
+                outputs['tsdf'] = pre_tsdf
+
+        return outputs, loss_dict
+
+    @staticmethod
+    def compute_loss(tsdf, occ, tsdf_target, occ_target, loss_weight=(1, 1),
+                     mask=None, pos_weight=1.0):
+        '''
+
+        :param tsdf: (Tensor), predicted tsdf, (N, 1)
+        :param occ: (Tensor), predicted occupancy, (N, 1)
+        :param tsdf_target: (Tensor),ground truth tsdf, (N, 1)
+        :param occ_target: (Tensor), ground truth occupancy, (N, 1)
+        :param loss_weight: (Tuple)
+        :param mask: (Tensor), mask voxels which cannot be seen by all views
+        :param pos_weight: (float)
+        :return: loss: (Tensor)
+        '''
+        # compute occupancy/tsdf loss
+        tsdf = tsdf.view(-1)
+        occ = occ.view(-1)
+        tsdf_target = tsdf_target.view(-1)
+        occ_target = occ_target.view(-1)
+        if mask is not None:
+            mask = mask.view(-1)
+            tsdf = tsdf[mask]
+            occ = occ[mask]
+            tsdf_target = tsdf_target[mask]
+            occ_target = occ_target[mask]
+
+        n_all = occ_target.shape[0]
+        n_p = occ_target.sum()
+        if n_p == 0:
+            logger.warning('target: no valid voxel when computing loss')
+            return torch.Tensor([0.0]).cuda()[0] * tsdf.sum()
+        w_for_1 = (n_all - n_p).float() / n_p
+        w_for_1 *= pos_weight
+
+        # compute occ bce loss
+        occ_loss = F.binary_cross_entropy_with_logits(
+            occ, occ_target.float(), pos_weight=w_for_1)
+
+        # compute tsdf l1 loss
+        tsdf = apply_log_transform(tsdf[occ_target])
+        tsdf_target = apply_log_transform(tsdf_target[occ_target])
+        tsdf_loss = torch.mean(torch.abs(tsdf - tsdf_target))
+
+        # compute Mean Planar Loss (for enforcing planarity) between matching voxels from estimated plane instances and tsdf
+
+        # compute final loss
+        loss = loss_weight[0] * occ_loss + loss_weight[1] * tsdf_loss
         return loss
